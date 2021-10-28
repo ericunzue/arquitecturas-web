3832bc809ef74ab2db808d4b29f330a9ff66e12e
ericunzue <ericunzue@gmail.com>
2021-10-28 19:14:35 -0300

    First commit

---
+++ TpIntegrador4/src/main/java/arq/integrador/despensa/TpIntegrador4Application.java
@@ -0,0 +1,13 @@
+package arq.integrador.despensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegrador4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegrador4Application.class, args);
+	}
+
+}
---
+++ TpIntegrador4/src/test/java/arq/integrador/despensa/TpIntegrador4ApplicationTests.java
@@ -0,0 +1,13 @@
+package arq.integrador.despensa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegrador4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
